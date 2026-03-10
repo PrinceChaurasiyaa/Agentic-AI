850911e77730030d3a0cd2fb23e850dff2ba1ead
PrinceChaurasiyaa <gravince.2.0@gmail.com>
2026-03-11 01:16:44 +0530

    Worked on Sidebar component

--- ChatBot/chatbot_sidebar.py
+++ ChatBot/chatbot_sidebar.py
@@ -12,19 +12,23 @@ def generate_thread_id():
 def reset_chat():
     thread_id = generate_thread_id()
     st.session_state['thread_id'] = thread_id
-    add_thread(st.session_state['thread_id'])
+    #add_thread(st.session_state['thread_id'], user_input)
     st.session_state.messages = []
 
-def add_thread(thread_id):
-    if thread_id not in st.session_state['chat_threads']:
-        st.session_state['chat_threads'].append(thread_id)
+def add_thread(thread_id, title=""):
+    if not any(t["id"] == thread_id for t in st.session_state['chat_threads']):
+        st.session_state['chat_threads'].append({
+            "id": thread_id,
+            "label": chat_label(),
+            "title": title[:12]
+        })
 
 def load_conversation(thread_id):
     return workflow.get_state(config={'configurable': {'thread_id': thread_id}}).values['messages']
 
 def chat_label():
     timestamp = datetime.now()
-    formated_time = timestamp.strftime("%d %b %Y, %I:%M:%S %p")
+    formated_time = timestamp.strftime("%d %b, %H:%M:%S")
     return formated_time
 
 # ===================================== Header UI =====================================================
@@ -46,7 +50,8 @@ if 'chat_threads' not in st.session_state:
 
 user_input = st.chat_input('How can I assists you ...')
 if user_input:
-    add_thread(st.session_state['thread_id'])
+    add_thread(st.session_state['thread_id'], user_input)
+
 
 # ==================================== Sidebar UI ======================================================
 with st.sidebar:
@@ -57,30 +62,42 @@ with st.sidebar:
 
     st.title("Your Converstions")
     for chat_thread in st.session_state['chat_threads'][::-1]:
-        if st.button(f"Chating: {chat_label()}", key=str(chat_thread)):
-            st.session_state['thread_id'] = chat_thread
-            messages = load_conversation(chat_thread)
+        if st.button(f"{chat_thread['title']}.. | {chat_thread['label']}", 
+                     key=str(chat_thread['id']), 
+                     use_container_width=True):
+            st.session_state['thread_id'] = chat_thread['id']
+            try:
+                messages = load_conversation(chat_thread['id'])
+                temp_messages = []
 
-            temp_messages = []
+                for msg in messages:
+                    if isinstance(msg, HumanMessage):
+                        role="user"
+                    else:
+                        role="assistant"
+                    temp_messages.append({"role": role, "content": msg.content})
+                st.session_state.messages = temp_messages
+            except:
+                st.error("No Chat Available.")
+                st.toast("No Chat Available.")
+                st.session_state['chat_error'] = True
 
-            for msg in messages:
-                if isinstance(msg, HumanMessage):
-                    role="user"
-                else:
-                    role="assistant"
-                temp_messages.append({"role": role, "content": msg.content})
-            st.session_state.messages = temp_messages
+            
 
 # ===================================== Main UI ======================================================
+if st.session_state.get('chat_error'):
+    st.info("Lets Start our First Conversions.")
+    st.session_state['chat_error'] = False
+
 for message in st.session_state.messages:
     with st.chat_message(message['role']):
-        st.text(message['content'])
+        st.markdown(message['content'])
 
 if user_input:
     # User side handling
     st.session_state.messages.append({"role": "user" ,"content": user_input})
     with st.chat_message('user'):
-        st.text(user_input)
+        st.markdown(user_input)
     
     # Gravince Side Handling
     CONFIG = {'configurable': {'thread_id': st.session_state['thread_id']}}
